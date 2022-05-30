fc0bd12a6fb5a3bf5988bc6dce075658b2fd8a4f
Denisa Haka <denisa.haka@ymail.com>
2022-05-30 21:50:21 +0100

    populated product component

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,25 @@
-﻿import React from 'react'
+﻿import React from 'react';
+import Link from 'next/link';
 
-const Product = () => {
+import { urlFor } from '../lib/client';
+
+//destructure some of the qualities of the product prop
+const Product = ({product: {name, image, slug, price}}) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`product/${slug.current}`}>
+        <div className='product-card'>
+          <img src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            alt="xbox series"
+            className='product-image'
+          />
+          <p className='product-name'>{name}</p>
+          <p className='product-price'>£{price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
