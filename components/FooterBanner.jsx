19a2134f60f5c3ce6155891f7574f3895ba24dcf
Denisa Haka <denisa.haka@ymail.com>
2022-05-30 20:00:37 +0100

    installed dependencies/ created schemas

---
+++ components/FooterBanner.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+
+const FooterBanner = () => {
+  return (
+    <div>FooterBanner</div>
+  )
+}
+
+export default FooterBanner;
---
+++ components/Product.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+
+const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
