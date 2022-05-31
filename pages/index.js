f1ae00d2eedfd1f1d9827cb1377501419b33be7d
Denisa Haka <denisa.haka@ymail.com>
2022-05-31 11:52:08 +0100

    fixed getStaticPaths bug and syntax bug

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,21 @@ import { client } from "../lib/client";
 
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = ({products, bannersData}) => {
+const Home = ({ products, bannersData }) => {
   return (
     <div>
       <>
         <HeroBanner heroBanner={bannersData.length && bannersData[0]} />
-        {console.log(bannersData)}
-
+       
 
         <div className="products-heading">
           <h2>Best Seller Products</h2>
           <p>Gaming Products</p>
         </div>
         <div className="products-container">
-          {products?.map((product) => <Product key={product._id} product={product} />)}
+          {products?.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
         </div>
 
         <FooterBanner footerBanner={bannersData && bannersData[0]} />
@@ -29,14 +30,13 @@ const Home = ({products, bannersData}) => {
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
- 
+
   const bannerQuery = '*[_type == "banner"]';
   const bannersData = await client.fetch(bannerQuery);
 
   return {
-    props: { products, bannersData }
-}
-
-}
+    props: { products, bannersData },
+  };
+};
 
 export default Home;
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,56 @@
-﻿import React from 'react'
+﻿import React, {useState} from "react";
+import { client, urlFor } from "../../lib/client";
 
-const ProductDetails = () => {
+//making an API call to fetch the desired product by using getStaticProps to pre-render the page
+const ProductDetails = ({ product, products }) => {
+    const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
+
   return (
-    <div> Product</div>
-  )
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src={urlFor(image && image[index])} className='product-detail-image' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+// [slug] is going to be dynamic and we'll have access
+//getStaticProps - getStaticPaths bug - every product needs to be clicked and immediately show the data for the current slug
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`;
+
+    const products = await client.fetch(query);
+    const paths = products.map((product) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }));
+
+    return {
+        paths,
+        fallback:'blocking'
+    }
 }
 
-export default ProductDetails
+export const getStaticProps = async ({ params: { slug }}) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
+
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+
+  return {
+    props: { product, products }, //will be passed to the page component as props
+  };
+};
+
+export default ProductDetails;
