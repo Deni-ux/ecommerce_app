0178cd9063c51880a9f04929497c0aa64d2d7c9b
Denisa Haka <denisa.haka@ymail.com>
2022-06-03 14:45:35 +0100

    cart show items and total price

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,78 +1,85 @@
-﻿import React, {createContext, useContext, useState, useEffect} from 'react'
-import { toast } from 'react-hot-toast';
-
+﻿import React, { createContext, useContext, useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
 
 //hooks
 const Context = createContext();
 
 //in our context we'll have a lot of states
-//including local storage so the user's data can be saved 
+//including local storage so the user's data can be saved
 export const StateContext = ({ children }) => {
-    const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState([]);
-    const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
-    const [qty, setQty] = useState(1);
-//update quantity using a previous state and callback function
-    const incQty = () => {
-        setQty((prevQty) => prevQty + 1);
-    }
-    const decQty = () => {
-        setQty((prevQty) => {
-            if (prevQty - 1 < 1) return 1;
-            return prevQty - 1;
-        });
-    }
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
+  const [qty, setQty] = useState(1);
 
-//add to cart function: 1- check if product already in the cart?
-    const onAdd = (product, quantity) => {
-        const checkProductInCart = cartItems.find((item) => item._id = product._id);
-        
-        //update states
-        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
-        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
-        
-        if (checkProductInCart) {
-         //update carts if the item already exists in the cart
-            const updatedCartItems = cartItem.map((cartProduct) => {
-                if (cartProduct._id === product._id) {
-                    return {
-                        ...cartProduct,
-                        quantity: cartProduct.quantity + quantity
-                    }
-                }
-            })
-            setCartItems(updatedCartItems);
-           
+  //update quantity using a previous state and callback function
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+  const decQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+      return prevQty - 1;
+    });
+  };
+
+  //add to cart function: 1- check if product already in the cart?
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => (item._id = product._id)
+    );
+
+    //update states
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkProductInCart) {
+      //update carts if the item already exists in the cart
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
         }
-        //if item doesn't exist in the cart
-        // ... is spread
-        else {
-            product.quantity = quantity;
-
-            setCartItems([...cartItems, { ...product }]);
-        }
-
-        toast.success(`${qty} ${product.name} added to cart.`);
+      });
+      setCartItems(updatedCartItems);
     }
+    //if item doesn't exist in the cart
+    // ... is spread
+    else {
+      product.quantity = quantity;
+
+      setCartItems([...cartItems, { ...product }]);
+    }
+
+    toast.success(`${qty} ${product.name} added to cart.`);
+  };
 
   return (
-      <Context.Provider
-          value={{
-              showCart,
-              cartItems,
-              totalPrice,
-              totalQuantities,
-              qty,
-              incQty,
-              decQty,
-              onAdd
-        }}
-      >
-        {children}  
+    <Context.Provider
+      value={{
+        showCart,
+        setShowCart,
+        cartItems,
+        setCartItems,
+        totalPrice,
+        setTotalPrice,
+        totalQuantities,
+        setTotalQuantities,
+        qty,
+        incQty,
+        decQty,
+        onAdd,
+      }}
+    >
+      {children}
     </Context.Provider>
-  )
-}
+  );
+};
 
 //create a new function to export all props and data more easily
 export const useStateContex = () => useContext(Context);
