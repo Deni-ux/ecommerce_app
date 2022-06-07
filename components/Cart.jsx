ec90c7f349283d8d5acae7246eca5b44b1cf879e
Denisa Haka <denisa.haka@ymail.com>
2022-06-07 13:03:50 +0100

    implemented stripe payments method

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -6,6 +6,7 @@ import toast from 'react-hot-toast';
 
 import { useStateContex } from '../context/StateContext';
 import { urlFor } from '../lib/client';
+import getStripe from '../lib/getStripe';
 
 
 
@@ -13,93 +14,113 @@ const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContex();
 
+  //Create an instance of checkout by making an API call
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
 
-  return (
-    <div className='cart-wrapper' ref={cartRef}>
-      <div className='cart-container'>
-        <button type='button' className='cart-heading' onClick={() => setShowCart(false)}>
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if(response.statusCode === 500) return;
+    
+    const data = await response.json();
+
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  }
+
+    return (
+      <div className='cart-wrapper' ref={cartRef}>
+        <div className='cart-container'>
+          <button type='button' className='cart-heading' onClick={() => setShowCart(false)}>
           
-          <AiOutlineLeft />
-          <span className='heading'>Your Cart</span>
-          <span className='cart-num-items'>({totalQuantities} items)</span>
-        </button>
+            <AiOutlineLeft />
+            <span className='heading'>Your Cart</span>
+            <span className='cart-num-items'>({totalQuantities} items)</span>
+          </button>
         
-        {/* show cart items  
+          {/* show cart items  
         1- if there is nothing:  */}
         
-        {cartItems.length < 1 && (
-          <div className='empty-cart'>
-            <AiOutlineShopping size={150} />
-            <h3>Your shopping bag is empty</h3>
+          {cartItems.length < 1 && (
+            <div className='empty-cart'>
+              <AiOutlineShopping size={150} />
+              <h3>Your shopping bag is empty</h3>
 
-            <Link href="/">
-              <button type='button' className='btn' onClick={() => setShowCart(false)} >
-                Continue Shopping
-              </button>
-            </Link>
-          </div>
-        )}
+              <Link href="/">
+                <button type='button' className='btn' onClick={() => setShowCart(false)} >
+                  Continue Shopping
+                </button>
+              </Link>
+            </div>
+          )}
 
-        {/* if there are products in the cart */}
-        <div className='product-container'>
-          {cartItems.length >= 1 && cartItems.map((item) => (
-            <div className='product' key={item._id}>
-              <img src={urlFor(item?.image[0])} className="cart-product-image" />
-              <div className='item-desc'>
-                <div className='flex top'>
-                  <h5>{item.name}</h5>
-                  <h4>£{item.price}</h4>
-                </div>
-
-          {/* Change Quantity */}
-          <div className='flex bottom'>
-                  <div>
-                    <p className='quantity-desc'>
-                      <span className="minus" onClick={() => toggleCartItemQuantity(item._id, 'dec')}>
-                <AiOutlineMinus />
-              </span>
-              <span className="num">
-                {`${item.quantity}`}
-              </span>
-              <span className="plus" onClick={() => toggleCartItemQuantity(item._id, 'inc')}>
-                <AiOutlinePlus />
-              </span>
-                    </p>
+          {/* if there are products in the cart */}
+          <div className='product-container'>
+            {cartItems.length >= 1 && cartItems.map((item) => (
+              <div className='product' key={item._id}>
+                <img src={urlFor(item?.image[0])} className="cart-product-image" />
+                <div className='item-desc'>
+                  <div className='flex top'>
+                    <h5>{item.name}</h5>
+                    <h4>£{item.price}</h4>
                   </div>
-                  <button
-                    type="button"
-                    className='remove-item'
-                    onClick={() => onRemove(item)}>
+
+                  {/* Change Quantity */}
+                  <div className='flex bottom'>
+                    <div>
+                      <p className='quantity-desc'>
+                        <span className="minus" onClick={() => toggleCartItemQuantity(item._id, 'dec')}>
+                          <AiOutlineMinus />
+                        </span>
+                        <span className="num">
+                          {`${item.quantity}`}
+                        </span>
+                        <span className="plus" onClick={() => toggleCartItemQuantity(item._id, 'inc')}>
+                          <AiOutlinePlus />
+                        </span>
+                      </p>
+                    </div>
+                    <button
+                      type="button"
+                      className='remove-item'
+                      onClick={() => onRemove(item)}>
                     
-                    <TiDeleteOutline />
-                  </button>
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
                 </div>
               </div>
+            ))}
+          </div>
+
+
+          {/* total items   */}
+          {cartItems.length >= 1 && (
+            <div className='cart-bottom'>
+              <div className='total'>
+                <h3>Subtotal:</h3>
+                <h3>£{totalPrice}</h3>
+              </div>
+
+              <div className='btn-container'>
+                <button type='button' className='btn' onClick={handleCheckout}>
+                  Pay with Stripe
+                </button>
+              </div>
             </div>
-          ))}
+          )}
+
         </div>
 
-
-      {/* total items   */}
-        {cartItems.length >= 1 && (
-          <div className='cart-bottom'>
-            <div className='total'>
-              <h3>Subtotal:</h3>
-              <h3>£{totalPrice}</h3>
-            </div>
-
-            <div className='btn-container'>
-              <button type='button' className='btn' onClick="">
-                Pay with Stripe
-              </button>
-            </div>
-          </div>
-        )}
-
       </div>
-
-    </div>
-  )
-}
+    )
+  }
 
 export default Cart;
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,39 +1,58 @@
-﻿import Stripe from 'stripe';
+﻿import Stripe from "stripe";
 
-const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //must have handler
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-      try {
-          const params = {
-            submit_type: "pay",
-            mode: "payment",
-            payment_method_types: ["card"],
-            billing_address_collection: "auto",
-            shipping_options: [
-              { shipping_rate: 'shr_1L720BLYq4xeuRnXNAI2SwKw'},
-            ],
-            line_items: [
-              {
-                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                price: "{{PRICE_ID}}",
-                quantity: 1,
-              },
-            ],
+  if (req.method === "POST") {
+    try {
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [{ shipping_rate: "shr_1L720BLYq4xeuRnXNAI2SwKw" }],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImg = img
+            .replace(
+              "image-",
+              "htps://cdn.sanity.io/images/ywelo34g/production/"
+            )
+            .replace("-png", ".png");
 
-            success_url: `${req.headers.origin}/?success=true`,
-            cancel_url: `${req.headers.origin}/?canceled=true`,
+          console.log("IMAGE", newImg);
+
+          return {
+            price_data: {
+              currency: "gbp",
+              product_data: {
+                name: item.name,
+                images: [newImg],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
           };
+        }),
+
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
   } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 }
-
